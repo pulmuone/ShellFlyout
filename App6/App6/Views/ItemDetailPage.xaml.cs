b795d9f071759f3f34d4789a940365ece0dead44
gwise <pulmuone.ji@gmail.com>
2020-12-18 22:00:58 +0900

    Add project files.

---
+++ App6/App6/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using App6.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace App6.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ App6/App6/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using App6.Models;
+using App6.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App6.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
